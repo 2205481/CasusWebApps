7f119395abf6f1c258b8c8e7634dacca11b2e201
Thomas Palmen <121097637+thmaz@users.noreply.github.com>
2023-11-01 11:20:33 +0100

    Initialised database

---
+++ Models/ImageHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace CasusWebApps.Models
+{
+    public class ImageHandler
+    {
+        public Guid Id {  get; set; }
+        public string ImageUrl { get; set; }
+        public string ItemType { get; set; }
+
+    }
+}
---
+++ Models/WasteDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CasusWebApps.Data;
+using CasusWebApps.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CasusWebApps.Models
+{
+    public class WasteDbContext : DbContext
+    {
+        public WasteDbContext(DbContextOptions<WasteDbContext> options) : 
+            base(options) 
+        {
+        
+        }
+
+        public DbSet<ImageHandler> ImageHandlers {  get; set; }
+
+    }
+}
